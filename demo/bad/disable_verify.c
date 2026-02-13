f989abfdcf265a470399948dd40c26f5a439b794
ChandlerCamarena <chandler.camarena@cox.net>
2026-02-13 13:30:49 +0100

    Remove ModuleLoadedCheck sanity test and finalize T1 implementation

--- demo/bad/disable_verify.c
+++ demo/bad/disable_verify.c
@@ -1,7 +1,7 @@
 #include <openssl/ssl.h>
 
-void good_verify_peer(SSL_CTX *ctx) {
-    /* T1 good: enable peer verification */
-    SSL_CTX_set_verify(ctx, SSL_VERIFY_PEER, 0);
+void bad_disable_verify(SSL_CTX *ctx) {
+    /* T1: certificate verification explicitly disabled */
+    SSL_CTX_set_verify(ctx, SSL_VERIFY_NONE, 0);
 }
 
